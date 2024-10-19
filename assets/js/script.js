49db5a747fcfbe6aa483eb8dd0356fd9d49309ab
Alejandro Di Stefano <addistefano@live.com.ar>
2024-10-19 12:29:09 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+// Variable
+// Tipos de Variables
+// pueden ser constantes o variables
+
+// Tipos de Datos
+// string - int - float - boolean
+
+// let alumno = "Alumno de Coder"; // int
+// console.log(alumno);
+// console.log(typeof(alumno));
+// // Reasignar un valor a alumno
+// alumno = "Un String"; // string
+// console.log(alumno);
+// console.log(typeof(alumno));
+// alumno = 23; // int
+// console.log(alumno);
+// console.log(typeof(alumno));
+
+
+// const constante = 123;
+// console.log(constante);
+// console.log(typeof(constante));
+// // reasiganr un nuevo valor a constante
+// //constante = 2; /// NO SE PUEDE REASIGNAR..!!!
+
+// let numeroFloat = 12.5;
+// console.log(numeroFloat);
+// console.log(typeof(numeroFloat));
+
+// let verdadero = true;
+// console.log(verdadero);
+// console.log(typeof(verdadero));
+
+/// Operaciones Matematicas
+let numeroA = 10;
+let numeroB = 2;
+
+// let suma = numeroA + numeroB; // Operacion suma
+// let resta = numeroA - numeroB; // Operacion resta
+// let multiplicacion = numeroA * numeroB; // Operacion multiplicacion
+// let division = numeroA / numeroB; // Operacion division
+
+// console.log(suma);
+// console.log(resta);
+// console.log(multiplicacion);
+// console.log(division);
+
+// let resultado;
+
+// resultado = numeroA + numeroB; 
+// console.log(resultado);
+// resultado = numeroA - numeroB;
+// console.log(resultado);
+// resultado = numeroA * numeroB;
+// console.log(resultado);
+// resultado = numeroA / numeroB;
+// console.log(resultado);
+
+
+console.log(numeroA + numeroB);
+console.log(numeroA - numeroB);
+console.log(numeroA * numeroB);
+console.log(numeroA / numeroB);
+
