1e218a64a627034ec7da980621d0bc95f2f28807
Alejandro Di Stefano <addistefano@live.com.ar>
2024-11-23 13:26:57 -0300

    Clase 06

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,162 +1,226 @@
-// Objetos
-// const persona1 = {
-//     id:1,
-//     nombre: "Francisco",
-//     apellido: "Arancibia",
-//     edad: 20,
-//     email: "francisco@email.com"
-// }
-// const persona2 = {
-//     nombre: "Gaston",
-//     apellido: "Zarate",
-//     edad: 22,
-//     email: "gaston@email.com"
+// Clase 06
+
+// Funciones de Orden Superior
+
+// function operarNumero(a, b, operacion) {
+//     return operacion(a, b);
 // }
 
-// console.log(persona1)
-// console.log(persona2)
+// const operarNumero = (a, b, operacion) => operacion(a, b);
 
-// function Persona(persona){ // Pasandole el Objeto Literal
-//     this.id = persona.id;
-//     this.nombre = persona.nombre;
-//     this.apellido = persona.apellido;
-//     this.edad = persona.edad;
-//     this.email = persona.email;
+// const suma = (x, y) => x + y;
+// const resta = (x, y) => x - y;
+// const multiplicacion = (x, y) => x * y;
+// const division = (x, y) => {
+//     if (y === 0) {
+//         return "Error: Division por cero no permitida.!!"
+//     }
+//     return x / y;
 // }
 
-// const personaLiteral = new Persona({
-//     id:1,
-//     nombre: "Francisco",
-//     apellido: "Arancibia",
-//     edad: 20,
-//     email: "francisco@email.com"
+// console.log(operarNumero(5, 61, suma));
+
+// console.log(operarNumero(5, 61, resta));
+
+// console.log(operarNumero(5, 61, multiplicacion));
+
+// console.log(operarNumero(5, 61, division));
+
+// console.log(operarNumero(5, 0, division));
+
+
+// Arrays
+
+// const myArray = [1, 2, 3];
+// console.log(myArray[1]);
+
+// const stringArray = ["Hola", "Mundo", "Coder"];
+// // console.log(stringArray);
+// stringArray.push("JavaScript") // Lo agrego al final del Array
+// console.log(stringArray.sort()); // Lo ordeno Alfabeticamente
+
+
+// const myArray = [1, 22, 3, 100, 520, -10, 0, 1002];
+// const ordenAs = (a, b) => a - b;
+// const ordenDes = (a, b) => b - a;
+/**
+ * si retorna un numero < 0 => a lo coloca antes que a b
+ * si retorna un numero > 0 => b lo coloca antes que a a
+ * si retorna un 0 => el orden entre ellos no cambia
+ */
+
+// myArray.sort(ordenAs);
+// console.log(myArray);
+// myArray.sort(ordenDes);
+// console.log(myArray);
+
+
+// foreach
+// const myArray = [1, 22, 3, 100, 520, -10, 0, 1002];
+
+// myArray.forEach((num) => {
+//     console.log(num);
 // })
-// console.log(personaLiteral)
 
-// Funciones Constructoras
-// function Persona(id, nombre, apellido, edad, email){
-//     this.id = id;
-//     this.nombre = nombre;
-//     this.apellido = apellido;
-//     this.edad = edad;
-//     this.email = email;
-// }
-
-// const francisco = new Persona(1, "Francisco", "Arancibia", 20, "francisco@mail.com ");
-// console.log(francisco);
-
-// francisco.nombre = "Francisco David";
-// francisco["edad"] = 22;
-
-// function Gato(nombre, edad, raza, color){
-//     this.nombre = nombre;
-//     this.edad = edad;
-//     this.raza = raza;
-//     this.color = color;
-// }
-
-// function Perro(nombre, edad, raza, color){
-//     this.nombre = nombre;
-//     this.edad = edad;
-//     this.raza = raza;
-//     this.color = color;
-// }
-
-// const gatito1 = new Gato("Michi", 2, "Cacrico", "Blanco");
-// const gatito2 = new Gato("Tomy", 2, "Siames", "Gris");
-
-// const perrito1 = new Perro("Daisy", 4, "Yorky", "Beige");
-
-// console.log(gatito1);
-// console.log(gatito2);
-// console.log(perrito1);
+// myArray.forEach((num, i) => {
+//     console.log(`Indice: ${i}, el Número es: ${num}`);
+// })
 
 
-// function caminar(){ // Funcion es Global y es accesible desde cualquier lado
-//     console.log("Mi Animal esta caminado.!");
-// }
+// myArray.forEach((num, i, array) => {
+//     array[i] = num * 2;
+// })
 
-// class Animal {
-//     constructor(tipo, nombre, edad, raza, color) {
-//         this.tipo = tipo;
-//         this.nombre = nombre;
-//         this.edad = edad;
-//         this.raza = raza;
-//         this.color = color;
+// console.log(myArray);
+
+
+// const stringArray = ["Hola", "Mundo", "Coder"];
+// const inventario = {};
+
+// stringArray.forEach((palabra, indice) => {
+//     inventario[indice] = palabra
+// })
+
+// console.log(inventario);
+
+// filter
+
+// const cursos = [
+//     {nombre: "JavaScript", precio: 150000},
+//     {nombre: "React", precio: 100000},
+//     {nombre: "Angular", precio: 110000},
+//     {nombre: "SQL", precio: 90000},
+//     {nombre: "BackEnd", precio: 350000},
+// ]
+// console.log(cursos);
+
+// const cursosBaratos = cursos.filter((curso) => curso.precio < 150000);
+// const cursosCaros = cursos.filter((curso) => curso.precio >= 150000);
+
+// console.log(cursosBaratos);
+// console.log(cursosCaros);
+
+
+// const myArray = [1, 22, 3, 100, 520, -10, 0, 1002];
+
+// const myArrayFiltrado = myArray.filter((num) => num > 10);
+// console.log(myArrayFiltrado);
+
+
+// const frutas = ["Manzana", "Frutilla", "Peras", "Anana", "Sandia", "Mandarina", "mango"];
+// const letra = 'M';
+
+// const frutasFiltradas = frutas.filter(fruta => fruta[0].toUpperCase() === letra.toUpperCase());
+
+// console.log(frutasFiltradas);
+
+// map
+
+// const frutas = ["Manzana", "Frutilla", "Peras", "Anana", "Sandia", "Mandarina", "mango"];
+// const frutasMayuscula = frutas.map((fruta) => fruta.toUpperCase());
+// console.log(frutas);
+// console.log(frutasMayuscula);
+
+// const myArray = [1, 22, 3, 100, 520, -10, 0, 1002];
+// const myArrayTriple = myArray.map((num) => num * 3);
+// console.log(myArray);
+// console.log(myArrayTriple);
+
+// const cursos = [
+//     {nombre: "JavaScript", precio: 150000},
+//     {nombre: "React", precio: 100000},
+//     {nombre: "Angular", precio: 110000},
+//     {nombre: "SQL", precio: 90000},
+//     {nombre: "BackEnd", precio: 350000},
+// ]
+// console.log(cursos);
+// const nombresDeCursos = cursos.map((curso) => curso.nombre);
+// console.log(nombresDeCursos);
+
+// const productos = [
+//     { id: 1, nombre: "Azucar", disponible: true },
+//     { id: 2, nombre: "Yerba", disponible: true },
+//     { id: 3, nombre: "Café", disponible: false },
+//     { id: 4, nombre: "Coca Cola", disponible: true },
+//     { id: 5, nombre: "Pepsi", disponible: false },
+//     { id: 6, nombre: "Harina", disponible: false },
+// ];
+
+// const booleanos = [true, false, false, false, true, true, false, true];
+// const texto = booleanos.map((valor) => {
+//     if (valor) {
+//         return "Si"
+//     } else {
+//         return "No"
+//     }
+// });
+
+// console.log(booleanos);
+// console.log(texto);
+
+// const stock = productos.map((producto) => {
+//     let disponibilidad = "";
+
+//     if(producto.disponible){
+//         disponibilidad = "tiene Stock";
+//     } else {
+//         disponibilidad = "No tiene Stock";
 //     }
 
-//     caminar() { // Es un metodo Propio de la Clase, y solo se accede desde cada instancia de Ella
-//         console.log("Mi " + this.tipo + " " + this.nombre + " esta caminado.!");
+//     return `El producto ${producto.nombre} ${disponibilidad}`;
+// })
+
+// console.log(stock);
+
+// Reduce
+
+// Ejemplo
+
+// const numeros = [1, 3, 6, 9, 12, 50, 88, 120, 1500]; // 1789
+
+// const sumaDeNumeros = numeros.reduce((acumulador, numero) => acumulador + numero, 0);
+// console.log(sumaDeNumeros);
+
+// const frutas = [
+//     "Manzana",
+//     "Frutilla",
+//     "Peras",
+//     "Anana",
+//     "Sandia",
+//     "Frutilla",
+//     "Mandarina",
+//     "mango",
+//     "Frutilla",
+//     "Mandarina",
+// ];
+
+// const conteo = frutas.reduce((acc, fruta) => {
+//     acc[fruta] = (acc[fruta] || 0) + 1;
+//     return acc;
+// }, {});
+// console.log(conteo);
+
+// const carrito = [
+//     {id: 1, nombre: "Manzana", cantidad: 3},
+//     {id: 2, nombre: "Frutilla", cantidad: 1},
+//     {id: 3, nombre: "Pera", cantidad: 5},
+//     {id: 4, nombre: "Mandarina", cantidad: 2},
+// ];
+
+// const resumenCarrito = carrito.reduce((acc, item) => {
+
+//     //si el producto ya existe en el acumulador
+//     if(acc[item.nombre]){
+//         acc[item.nombre].cantidad += item.cantidad;
+//     } else {
+//         acc[item.nombre] = {
+//             id: item.id,
+//             nombre: item.nombre,
+//             cantidad: item.cantidad
+//         }
 //     }
-// }
+//     return acc;
+// }, {});
 
-// const perrito = new Animal("Perro", "Daisy", 4, "Yorky", "Beige");
-// const gatito = new Animal("Gato", "Michi", 2, "Cacrico", "Blanco");
-// const gatit2 = new Animal("Gato", "Michifus", 2, "Cacrico", "Blanco y Negro");
-// const arrayAnimales = [perrito, gatito, gatit2]; // Array de Objetos
-// console.log(arrayAnimales)
-// console.log(perrito);
-// console.log(perrito.color);
-// console.log(gatito);
+// console.log(resumenCarrito);
 
-// perrito.caminar();
-// gatito.caminar();
-
-// caminar();
-
-// let mensaje = "Hola Soy mensaje1";
-
-// localStorage.setItem("mensaje1", mensaje);
-// localStorage.setItem("mensaje2", "Hola a todos desde el Local Storage");
-// sessionStorage.setItem("mensaje1", mensaje);
-// sessionStorage.setItem("mensaje2", "Hola a todos desde el Local Storage");
-
-// console.log(localStorage.getItem("mensaje1"));
-
-// localStorage.setItem("numero" , 1);
-// localStorage.setItem("boolean" , true);
-// localStorage.setItem("Perro", perrito);
-// localStorage.setItem("Perro", JSON.stringify(perrito)); // Convertirlo en String, para ello uso el Formato JSON
-// localStorage.setItem("Gato", JSON.stringify(gatito)); 
-// stringify convierte a un Objeto en formato String
-// console.log(typeof(perrito)); // Un Objeto
-// console.log(typeof(JSON.stringify(perrito))); // Es un String
-
-// const perroComoString = localStorage.getItem("Perro");
-// console.log(typeof(perroComoString)); // Es un String
-
-
-// const perroComoObject = JSON.parse(localStorage.getItem("Perro"));
-// console.log(typeof(perroComoObject));
-// console.log(perroComoObject);
-// console.log(perroComoObject.color);
-
-// localStorage.removeItem("numero");
-// localStorage.removeItem("boolean");
-
-// localStorage.clear();
-
-// console.log(typeof(arrayAnimales))
-
-// localStorage.setItem("Animales", JSON.stringify(arrayAnimales));
-
-// for (let i = 0; i < localStorage.length; i++){
-//     let clave = localStorage.key(i);
-//     console.log("Clave: " + clave + "\nValor: " + localStorage.getItem(clave));
-// }
-
-// const guardarEnElLocalStorage = (clave, valor) => {
-//     localStorage.setItem(clave, valor);
-// }
-
-// localStorage.setItem("Perro", JSON.stringify(perrito)); // Convertirlo en String, para ello uso el Formato JSON
-// localStorage.setItem("Gato", JSON.stringify(gatito)); 
-
-// localStorage.setItem("Animales", JSON.stringify(arrayAnimales));
-
-// guardarEnElLocalStorage("Perro", JSON.stringify(perrito));
-// guardarEnElLocalStorage("Gato", JSON.stringify(gatito));
-
-// for (const animal of arrayAnimales) {
-//     guardarEnElLocalStorage(animal.tipo, JSON.stringify(animal));
-// }
